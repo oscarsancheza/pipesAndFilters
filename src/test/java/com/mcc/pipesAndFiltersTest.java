1198a587b983a42329aa818fd19a2517b8a8acb4
oosancheza <oosancheza@concredito.com.mx>
2019-02-07 13:36:30 -0700

    [MOD] modificacion pruebas

--- src/test/java/com/mcc/pipesAndFiltersTest.java
+++ src/test/java/com/mcc/pipesAndFiltersTest.java
@@ -17,32 +17,29 @@ public class pipesAndFiltersTest {
 
   private KWIC kwic;
   private KWIC kwic2;
+  private CompletableFuture<String> future = new CompletableFuture<>();
+  private CompletableFuture<String> future2 = new CompletableFuture<>();
 
   @Before
   public void setUp() {
     kwic = new KWIC();
     kwic2 = new KWIC();
+    future = new CompletableFuture<>();
+    future2 = new CompletableFuture<>();
   }
 
   @Test
   public void isResultEqualToAnotherResult() throws ExecutionException, InterruptedException {
-    CompletableFuture<String> future = new CompletableFuture<>();
-    CompletableFuture<String> future2 = new CompletableFuture<>();
-
     kwic.execute(path, future::complete);
     kwic.execute(path, future2::complete);
-    Assert.assertEquals(future.get(),future2.get());
+    Assert.assertEquals(future.get(), future2.get());
   }
 
   @Test
   public void isResultNotEqualToAnotherResult() throws ExecutionException, InterruptedException {
-    CompletableFuture<String> future = new CompletableFuture<>();
-    CompletableFuture<String> future2 = new CompletableFuture<>();
-
     kwic.execute(path, future::complete);
     kwic2.execute(path2, future2::complete);
 
-    Assert.assertNotEquals(future.get(),future2.get());
-
+    Assert.assertNotEquals(future.get(), future2.get());
   }
 }
